2cf4da687d34c251b31d3216ac97b35c1254ad23
Yogeshb42 <yogeshbhagat4242@gmail.com>
2023-07-11 12:23:12 +0530

    app

--- app.py
+++ app.py
@@ -1,54 +1,94 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trip_wallet.db'
+db = SQLAlchemy(app)
 
-with app.app_context():
-    db = SQLAlchemy(app)
 
-class Todo(db.Model):
-    no = db.Column(db.Integer, primary_key = True)
-    title = db.Column(db.String, nullable = False)
-    desc = db.Column(db.String, nullable = False)
-    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    amount = db.Column(db.Float)
+    settlement = db.Column(db.String(255))
+    calculation = db.Column(db.Float, default=0)
 
-    def __repr__(self)-> str:
-        return f"{self.no} - {self.title}"
+    def __init__(self, name, amount):
+        self.name = name
+        self.amount = amount
+        self.settlement = ""
 
-@app.route('/', methods = ['GET', 'POST'])
-def hello_world():
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = Todo(title=title, desc=desc)
-        db.session.add(todo)
+        name = request.form['name']
+        amount = float(request.form['amount'])
+
+        entry = Entry(name=name, amount=amount)
+        db.session.add(entry)
         db.session.commit()
 
-    allTodo = Todo.query.all()
-    return render_template('index.html',allTodo=allTodo)
+        table_data = Entry.query.order_by(Entry.id).all()
+        update_settlement(table_data)  # Call update_settlement() with all entries
 
-@app.route('/delete/<int:no>')
-def delete(no):
-    todo = Todo.query.filter_by(no=no).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect ('/')
-
-@app.route('/update/<int:no>', methods = ['GET', 'POST'])
-def update(no):
-    if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = Todo.query.filter_by(no=no).first()
-        todo.title = title
-        todo.desc = desc
-        db.session.add(todo)
         db.session.commit()
-        return redirect("/")
 
-    todo = Todo.query.filter_by(no=no).first()
-    return render_template('update.html',todo=todo)
+    elif request.method == 'GET':
+        table_data = Entry.query.order_by(Entry.id).all()
 
+    for index, entry in enumerate(table_data, start=1):
+        entry.no = index
+
+    return render_template('index.html', table_data=table_data)
+
+
+@app.route('/update', methods=['POST'])
+def update_entry():
+    entry_id = int(request.form['id'])
+    new_amount = float(request.form['newAmount'])
+
+    entry = Entry.query.get(entry_id)
+    if entry:
+        entry.amount = new_amount
+        db.session.commit()
+
+        table_data = Entry.query.order_by(Entry.id).all()
+        update_settlement(table_data)  # Call update_settlement() with all entries
+
+        db.session.commit()
+
+    return jsonify({'message': 'Entry updated successfully'})
+
+
+def update_settlement(entries):
+    total_entries = len(entries)
+    total_amount = sum(entry.amount for entry in entries)
+    mean_amount = total_amount / total_entries
+
+    for entry in entries:
+        diff = mean_amount - entry.amount
+        entry.calculation = diff
+
+        if diff > 0:
+            entry.settlement = f"Pay {diff:.2f} to others"
+        elif diff < 0:
+            entry.settlement = f"Receive {-diff:.2f} from others"
+        else:
+            entry.settlement = ""
+
+
+@app.route('/delete', methods=['POST'])
+def delete_entry():
+    entry_id = int(request.form['id'])
+    entry = Entry.query.get(entry_id)
+    if entry:
+        db.session.delete(entry)
+        db.session.commit()
+
+        table_data = Entry.query.order_by(Entry.id).all()
+        update_settlement(table_data)  # Call update_settlement() with all entries
+
+        db.session.commit()
+
+    return jsonify({'message': 'Entry deleted successfully'})
